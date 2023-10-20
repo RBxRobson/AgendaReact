89ade4ea326ee18e23476a19d687be3bb26df1b1
Robson.B <robsonnbarbosa0@gmail.com>
2023-10-20 16:03:53 -0300

    commit inicial

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'plugin:prettier/recommended',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react-refresh', 'prettier', '@typescript-eslint'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+}
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import './App.css'
+import GlobalStyles from './styles'
+
+function App() {
+  return <GlobalStyles />
+}
+
+export default App
